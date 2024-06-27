7a7008a0f889485d088763059918ce463f448928
TMachadoDev <tiagomachado23012008@gmail.com>
2024-06-27 22:11:01 +0100

    change base url

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Replace with your API base URL
+  baseURL: 'https://deleted.discloud.app/api', // Replace with your API base URL
   timeout: 0, // Timeout of 5 seconds
 });
 
